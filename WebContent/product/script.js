51a146cc4d1997f8a0b0d48ad629dbf5e059eafb
s-oo <d-aoo@naver.com>
2022-08-05 14:31:48 +0900

    ✨ Add onchange event
    1. Show selectBox of size when selectBox of color is selected
    2. Show tbody of selectedOptions when selectBox of size is selected

---
+++ WebContent/product/script.js
@@ -0,0 +1,9 @@
+function select(option) {
+    
+   if(option=="case1"){
+	   document.getElementById("optionSize").style.display = "block";
+	}
+   if(option=="case2"){
+	   document.getElementById("selectedOptions").style.display = "block";
+	}
+}
